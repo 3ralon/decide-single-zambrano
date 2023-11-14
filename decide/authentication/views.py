f3ccee258befadff9b1bf41f1bd47e00882d4e56
MarioArocaPaez <mararopae@alum.us.es>
2023-11-14 20:46:40 +0100

    feat: Register para usuarios normales

--- decide/authentication/views.py
+++ decide/authentication/views.py
@@ -6,7 +6,10 @@ from rest_framework.status import (
 )
 from rest_framework.views import APIView
 from rest_framework.authtoken.models import Token
+from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 from django.db import IntegrityError
 from django.shortcuts import get_object_or_404
 from django.core.exceptions import ObjectDoesNotExist
@@ -35,21 +38,39 @@ class LogoutView(APIView):
 
 class RegisterView(APIView):
     def post(self, request):
-        key = request.data.get('token', '')
-        tk = get_object_or_404(Token, key=key)
-        if not tk.user.is_superuser:
-            return Response({}, status=HTTP_401_UNAUTHORIZED)
+        # Verificar si el usuario que hace la solicitud es un superusuario o un usuario normal
+        is_superuser = request.user.is_superuser
 
+        # Si no es un superusuario, manejar el registro del usuario normal
+        if not is_superuser:
+            form = UserCreationForm(request.data)
+            if form.is_valid():
+                user = form.save()
+                token, _ = Token.objects.get_or_create(user=user)
+                return Response({'user_pk': user.pk, 'token': token.key}, status=HTTP_201_CREATED)
+            else:
+                return Response(form.errors, status=HTTP_400_BAD_REQUEST)
+
+        # Si es un superusuario, manejar el registro del administrador
         username = request.data.get('username', '')
         pwd = request.data.get('password', '')
+
         if not username or not pwd:
             return Response({}, status=HTTP_400_BAD_REQUEST)
 
         try:
-            user = User(username=username)
-            user.set_password(pwd)
-            user.save()
-            token, _ = Token.objects.get_or_create(user=user)
+            user, created = User.objects.get_or_create(username=username)
+            if created:
+                user.set_password(pwd)
+                user.save()
+                token, _ = Token.objects.get_or_create(user=user)
+                return Response({'user_pk': user.pk, 'token': token.key}, status=HTTP_201_CREATED)
+            else:
+                return Response({'error': 'User already exists'}, status=HTTP_400_BAD_REQUEST)
+        
         except IntegrityError:
             return Response({}, status=HTTP_400_BAD_REQUEST)
-        return Response({'user_pk': user.pk, 'token': token.key}, HTTP_201_CREATED)
+
+    def get(self, request):
+        form = UserCreationForm()
+        return render(request, 'registration_form.html', {'form': form})
