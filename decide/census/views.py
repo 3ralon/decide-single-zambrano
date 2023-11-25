98396692f98046f0939e040e39818c46e854a4dc
claudia <clagilpri@alum.us.es>
2023-11-25 16:43:02 +0100

    Fix: Solo superusers puedes descargar censo #42
    Se ha añadido que solo los superusers se puedan descargar  el censo

--- decide/census/views.py
+++ decide/census/views.py
@@ -12,30 +12,80 @@ from rest_framework.status import (
         HTTP_401_UNAUTHORIZED as ST_401,
         HTTP_409_CONFLICT as ST_409)
 import csv
+from django.views import View
 from base.perms import UserIsStaff
 from .models import Census
 from django.http import HttpResponse
 from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+import csv
+from .models import Census
+
+class CensusExportationToCSV(View):     
+    
+
+    def get(self, request, *args, **kwargs):
+        voting_id = kwargs.get('voting_id')
+        if voting_id is not None:
+            census = Census.objects.filter(voting_id=voting_id)
+            filename = f"censo_{voting_id}.csv"
+        else:
+            census = Census.objects.all()
+            filename = "CensoCompleto.csv"
+
+        if not request.user.is_superuser:
+            return HttpResponse("Only superusers can download census data")
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = f'attachment; filename="{filename}"'
+
+        writer = csv.writer(response)
+        writer.writerow(['Voting', 'Voter'])
+        for profile in census:
+            writer.writerow([profile.voting_id, profile.voter_id])
+
+        return response
 
 
-class CensusExportationToCSV():     
-       
-    def export_voting_to_csv(request, voting_id):
+    def export_all_census(self, request):
+            if not request.user.is_superuser:
+                return HttpResponse("Only superusers can download census data")
+
+            census = Census.objects.all()
+            response = HttpResponse(
+            content_type = 'text/csv',
+            headers = {
+                "Content-Disposition": 'attachment; filename="CensoCompleto.csv"'
+                }
+            )
+            writer = csv.writer(response)
+            writer.writerow(['Voting','Voter'])
+            profile_fields = census.values_list('voting_id', 'voter_id')
+            for profile in profile_fields:
+                writer.writerow(profile)
+            return response 
+
+    def export_voting_to_csv(self, request, voting_id):
+        if not request.user.is_superuser:
+            return HttpResponse("Only superusers can download census data")
+
         census = Census.objects.filter(voting_id=voting_id)  
         response = HttpResponse(
-            content_type='text/csv',
-            headers={
-                "Content-Disposition": 'attachment; filename="censo.csv"'
-                }
-        )
+        content_type='text/csv',
+        headers={
+            "Content-Disposition": 'attachment; filename="censo.csv"'
+            } )
         writer = csv.writer(response)
         writer.writerow(['Voting', 'Voter'])
         for profile in census:
             writer.writerow([profile.voting_id, profile.voter_id])
         return response
     
-    def export_to_csv(request):
+    def export_to_csv(self, request):
+        if not request.user.is_superuser:
+            return HttpResponse("Only superusers can download census data")
+
         if request.method == 'GET':
             voting_id = request.GET.get('voting_id')
             if voting_id is not None:
@@ -56,29 +106,16 @@ class CensusExportationToCSV():
         else:
             return HttpResponse("Método de solicitud no válido")
 
-    def export_all_census(request):
-        census = Census.objects.all()
-        response = HttpResponse(
-            content_type = 'text/csv',
-            headers = {
-                "Content-Disposition": 'attachment; filename="CensoCompleto.csv"'
-                }
-            )
-        writer = csv.writer(response)
-        writer.writerow(['Voting','Voter'])
-        profile_fields = census.values_list('voting_id', 'voter_id')
-        for profile in profile_fields:
-            writer.writerow(profile)
-        return response 
-
-    def export_page(request):
-        return render(request, 'export_csv.html')
     
 
+   
+    
 class CensusCreate(generics.ListCreateAPIView):
-    permission_classes = (UserIsStaff,)
-
+    
     def create(self, request, *args, **kwargs):
+        if not request.user.is_superuser:
+            return Response('Only superusers can create censuses', status=status.HTTP_403_FORBIDDEN)
+
         voting_id = request.data.get('voting_id')
         voters = request.data.get('voters')
         try:
@@ -93,7 +130,6 @@ class CensusCreate(generics.ListCreateAPIView):
         voting_id = request.GET.get('voting_id')
         voters = Census.objects.filter(voting_id=voting_id).values_list('voter_id', flat=True)
         return Response({'voters': voters})
-
 
 class CensusDetail(generics.RetrieveDestroyAPIView):
 
