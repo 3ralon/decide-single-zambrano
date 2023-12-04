297224a7646fa483f1c365a157b669873ce8fc42
MarioArocaPaez <mararopae@alum.us.es>
2023-12-04 12:12:41 +0100

    Añadir test de selenium para register.
    Ahora mismo solo funcionan si estamos corriendo el servidor en el 8000 tengo que arreglarlo porque hay un problema con el site id, pero no lo consigo

--- decide/authentication/test_selenium.py
+++ decide/authentication/test_selenium.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.db import connection
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 from base.tests import BaseTestCase
@@ -8,6 +9,15 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
+from django.contrib.auth.models import User
+from selenium.webdriver.support.ui import WebDriverWait
+from django.contrib.sites.models import Site
+from django.contrib.sites.models import Site
+from django.contrib.sites.shortcuts import get_current_site
+from allauth.socialaccount.models import SocialApp
+from django.db import models
+from django.contrib.sites.models import Site
+from allauth.socialaccount.models import SocialApp
 
 
 class AdminTestCase(StaticLiveServerTestCase):
@@ -18,7 +28,7 @@ class AdminTestCase(StaticLiveServerTestCase):
 
         # Opciones de Chrome
         options = webdriver.ChromeOptions()
-        options.headless = True
+        options.headless = False
         self.driver = webdriver.Chrome(options=options)
 
         super().setUp()
@@ -48,6 +58,60 @@ class AdminTestCase(StaticLiveServerTestCase):
         # Si no, aparece este error
         self.assertTrue(len(self.driver.find_elements(By.CLASS_NAME, "errornote")) == 1)
         time.sleep(5)
+        
+class RegistrationTestCase(StaticLiveServerTestCase):
+    
+    def setUp(self):
+        self.base = BaseTestCase()
+        self.base.setUp()
+
+        options = webdriver.ChromeOptions()
+        options.headless = False
+        options.add_argument("--no-sandbox")
+        self.driver = webdriver.Chrome(options=options)
+        self.user = User.objects.create_user(username="testuser", password="testpass")
+
+        current_site = get_current_site(self.driver)
+        print(current_site)
+        
+        app = SocialApp.objects.create(
+            provider="Google",
+            name="google",
+            client_id="test",
+            secret="test",
+        )
+        app.sites.add(current_site)
+        
+        super().setUp()
+        
+    def tearDown(self):
+        super().tearDown()
+        self.driver.quit()
+        self.base.tearDown()
+        
+
+
+    def test_simpleCorrectRegistration(self):
+        
+        self.driver.get("http://localhost:8000/authentication/register/")
+        #self.driver.get(f"{self.live_server_url}/authentication/register/")
+        time.sleep(10)
+        username_input = self.driver.find_element(By.ID, "id_username")
+        username_input.send_keys("testuser")
+        password_input = self.driver.find_element(By.ID, "id_password1")
+        password_input.send_keys("D0z29'RM<I4m")
+        password_confirm_input = self.driver.find_element(By.ID, "id_password2")
+        password_confirm_input.send_keys("D0z29'RM<I4m")
+
+        # Submit the form
+        time.sleep(10)
+        self.driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
+
+        # Check if a new user was created with the correct username and password
+        self.assertEqual(User.objects.count(), 3)
+        user = User.objects.get(username="testuser")
+        self.assertTrue(user.check_password("D0z29'RM<I4m"))
+        
     
 
 class GoogleLoginTestCase(StaticLiveServerTestCase):
