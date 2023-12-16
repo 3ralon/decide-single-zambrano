a893af2961fe89fb6df31d0ff439af3a5128d3d7
claudia <clagilpri@alum.us.es>
2023-12-16 12:15:22 +0100

    FIx: Render #i109 Faltaba un middleware en local_settings_deploy.py cuerpo

--- decide/local_settings.deploy.py
+++ decide/local_settings.deploy.py
@@ -34,6 +34,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "allauth.account.auth_backends.AuthenticationBackend",
 ]
 
 STATIC_URL = "/static/"
