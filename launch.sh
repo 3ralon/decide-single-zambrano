7a5cbf22865bda3a6313437443a2941bdc55428e
claudia <clagilpri@alum.us.es>
2023-12-16 10:53:01 +0100

    Fix: Render #109

--- launch.sh
+++ launch.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-cd decide/
+#cd decide/
 cp local_settings.deploy.py local_settings.py
 ./manage.py createsuperuser --noinput
 ./manage.py collectstatic --noinput
 ./manage.py makemigrations
 ./manage.py migrate
-gunicorn -w 5 decide.wsgi:application --timeout=500
-‎
+gunicorn -w 5 decide.decide.wsgi:application --timeout=500
+
